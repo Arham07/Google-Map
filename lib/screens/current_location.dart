2296e97bceb2a6949ad7ab166c0b46e438a8d8fb
Arham07 <arhamj0726@gmail.com>
2022-06-25 20:04:13 +0500

    location details added

--- lib/screens/current_location.dart
+++ lib/screens/current_location.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -11,6 +12,9 @@ class CurrentLocation extends StatefulWidget {
 }
 
 class _CurrentLocationState extends State<CurrentLocation> {
+
+  String address='';
+
   static const CameraPosition _kGooglePlex =
       CameraPosition(target: LatLng(24.968908, 67.064789), zoom: 14.0);
 
@@ -42,66 +46,108 @@ class _CurrentLocationState extends State<CurrentLocation> {
     return await Geolocator.getCurrentPosition();
   }
 
-  loadData(){
-    getCurrentLocation().then((value) async {
-      _marker.add(
-          Marker(
-              markerId: const MarkerId('2'),
-              position: LatLng(value.latitude ,value.longitude),
-              infoWindow:  const InfoWindow(
-                  title: 'My current Location'
-              )
-          )
-      );
-
-      final GoogleMapController controller = await _controller.future;
-      CameraPosition _kGooglePlex =  CameraPosition(
-        target: LatLng(value.latitude ,value.longitude),
-        zoom: 14,
-      );
-      controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex));
-      setState(() {
-
-      });
-    });
-  }
+  // loadData() {
+  //   getCurrentLocation().then((value) async {
+  //     _marker.add(Marker(
+  //         markerId: const MarkerId('2'),
+  //         position: LatLng(value.latitude, value.longitude),
+  //         infoWindow: const InfoWindow(title: 'My current Location')));
+  //
+  //     final GoogleMapController controller = await _controller.future;
+  //     CameraPosition kGooglePlex = CameraPosition(
+  //       target: LatLng(value.latitude, value.longitude),
+  //       zoom: 14,
+  //     );
+  //     controller.animateCamera(CameraUpdate.newCameraPosition(kGooglePlex));
+  //     setState(() {});
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GoogleMap(
-          initialCameraPosition: _kGooglePlex,
-          markers: Set<Marker>.of(_marker),
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
+        title: const Text('Flutter Google Map'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.location_disabled_outlined),
-        onPressed: () async {
-          getCurrentLocation().then((value) async {
-            print('My current location');
-            print(value.latitude.toString());
-            _marker.add(
-              Marker(
-                  markerId: const MarkerId('2'),
-                  position: LatLng(value.latitude, value.longitude),
-                  infoWindow: const InfoWindow(title: 'My Current Location')),
-            );
-            CameraPosition cameraPosition = CameraPosition(
-              target: LatLng(value.latitude, value.longitude),
-              zoom: 14,
-            );
-            GoogleMapController controller = await _controller.future;
-            controller.animateCamera(
-              CameraUpdate.newCameraPosition(cameraPosition),
-            );
-          setState(() {});
-          });
-        },
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            GoogleMap(
+              initialCameraPosition: _kGooglePlex,
+              mapType: MapType.normal,
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              markers: Set<Marker>.of(_marker),
+              onMapCreated: (GoogleMapController controller){
+                _controller.complete(controller);
+              },
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * .15,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: (){
+                      getCurrentLocation().then((value) async {
+                        print('My current location');
+                        print(value.latitude.toString());
+                        _marker.add(
+                          Marker(
+                              markerId: const MarkerId('2'),
+                              position: LatLng(value.latitude, value.longitude),
+                              infoWindow: const InfoWindow(title: 'My Current Location')),
+                        );
+                        CameraPosition _kGooglePlex = CameraPosition(
+                          target: LatLng(value.latitude, value.longitude),
+                          zoom: 14,
+                        );
+                        GoogleMapController controller = await _controller.future;
+                        controller.animateCamera(
+                          CameraUpdate.newCameraPosition(_kGooglePlex),
+                        );
+                        List<Placemark> placemarks = await placemarkFromCoordinates(value.latitude ,value.longitude);
+
+
+                        final add = placemarks.first;
+                        address = "${add.name} ${add.subAdministrativeArea} ${add.administrativeArea} ${add.country}";
+
+                        setState(() {});
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10 , horizontal: 20),
+                      child: Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.deepOrange,
+                            borderRadius: BorderRadius.circular(8)
+                        ),
+                        child: const Center(child: Text('Current Location' , style: TextStyle(color: Colors.white),)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(address),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+
       ),
     );
   }
+
+
 }
